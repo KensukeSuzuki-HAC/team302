400808336cf9b7b4ee10084c4cc00fbf20f8e13e
SeitaTakehana-HAC <122654150+SeitaTakehana-HAC@users.noreply.github.com>
2024-10-04 15:28:26 +0900

    竹花
    Playerの改造

--- nanntonaku/Assets/Player.cs
+++ nanntonaku/Assets/Player.cs
@@ -4,55 +4,81 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public bool isWallHit_R;//右壁接触のフラグ
-    public bool isWallHit_L;//左壁接触のフラグ
-    public bool isWallHit_U;//上壁接触のフラグ
-    public bool isWallHit_D;//下壁接触のフラグ
-    public bool isGravity;//重力反転のフラグ
-    public float xSpeed = 3.0f;//横方向の移動スピード
-    public float ySpeed = 60.0f;//上方向の移動スピード
+    public bool isWallHit_R;                //右壁接触のフラグ
+    public bool isWallHit_L;                //左壁接触のフラグ
+    public bool isWallHit_U;                //上壁接触のフラグ
+    public bool isWallHit_D;                //下壁接触のフラグ
+    public bool isWallHit_middleU;          //中層上壁接触のフラグ
+    public bool isWallHit_middleD;          //中層下壁接触のフラグ
+    public bool isGravity;                  //重力反転のフラグ
+    public bool isFalling;                  //落下のフラグ
+    public float xSpeed = 3.0f;             //横方向の移動スピード
+    public float ySpeed = 60.0f;            //上方向の移動スピード
     // Start is called before the first frame update
     void Start()
     {
-        isWallHit_R = false;    //初期化
-        isWallHit_L = true;	    //初期化
-        isWallHit_U = false;    //初期化
-        isWallHit_D = true;    //初期化
-        isGravity= false;//初期化
+        isWallHit_R = false;            //初期化
+        isWallHit_L = true;	            //初期化
+        isWallHit_U = false;            //初期化
+        isWallHit_D = true;             //初期化
+        isWallHit_middleU = false;      //初期化
+        isWallHit_middleD = false;      //初期化
+        isGravity = false;              //初期化
+        isFalling = false;              //初期化
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (isWallHit_R == true)
+        if (isWallHit_R == true)//右壁についたとき
         {
             //左移動
             transform.position -= transform.right * xSpeed * Time.deltaTime;
         }
-        if (isWallHit_L == true)
+        if (isWallHit_L == true)//左壁についたとき
         {
            //右移動
             transform.position += transform.right * xSpeed * Time.deltaTime;
         }
-        if (Input.GetKeyDown(KeyCode.Space))//スペースキーが押されたら
+        if (Input.GetKeyDown(KeyCode.Space))//スペースキーが押されたとき
         {
-            isGravity = true;
+            if (isFalling == false)//落下フラグが立ってなければ
+            {
+                isGravity = true;//重力反転フラグを立てる
+            }
         }
-        if (isGravity == true)
+        if (isGravity == true)//重力反転フラグが立ったとき
         {
-            if (isWallHit_U == true)
+            if (isWallHit_U == true|| isWallHit_middleU==true)//上壁についていれば
             {
                 //下移動
                 transform.position -= transform.forward * ySpeed * Time.deltaTime;
                
             }
 
-            if (isWallHit_D == true)
+            if (isWallHit_D == true|| isWallHit_middleD==true)//下壁についていれば
             {
                 //上移動
                 transform.position += transform.forward * ySpeed * Time.deltaTime;
                 
             }
+        }
+        if(isFalling == true)//落下フラグが立ったとき
+        {
+            if (isGravity == false)//重力反転フラグが立っていなければ
+            {
+                if (isWallHit_middleU == true)
+                {
+                    //上移動
+                    transform.position += transform.forward * ySpeed * Time.deltaTime;
+                }
+
+                if (isWallHit_middleD == true)
+                {
+                    //下移動
+                    transform.position -= transform.forward * ySpeed * Time.deltaTime;
+                }
+            }
+            
         }
     }
 
@@ -72,13 +98,45 @@ public class Player : MonoBehaviour
         {
             isWallHit_U = true;
             isWallHit_D = false;
+            isWallHit_middleU = false;
+            isWallHit_middleD = false;
             isGravity = false;
+            isFalling = false;
         }
         if (collision.gameObject.CompareTag("Wall_D"))
         {
             isWallHit_U = false;
             isWallHit_D= true;
+            isWallHit_middleU = false;
+            isWallHit_middleD = false;
             isGravity = false;
+            isFalling = false;
         }
+        if (collision.gameObject.CompareTag("Wall_middleU"))
+        {
+            isWallHit_U = false;
+            isWallHit_D = false;
+            isWallHit_middleU = true;
+            isWallHit_middleD = false;
+            isGravity = false;
+            isFalling = false;
+        }
+        if (collision.gameObject.CompareTag("Wall_middleD"))
+        {
+            isWallHit_U = false;
+            isWallHit_D = false;
+            isWallHit_middleU = false;
+            isWallHit_middleD = true;
+            isGravity = false;
+            isFalling = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+       if(other.gameObject.CompareTag("Wall_middleU")|| other.gameObject.CompareTag("Wall_middleD"))
+       {
+            isFalling = true;
+       }
     }
 }
